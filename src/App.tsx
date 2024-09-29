4f6c97f1e3b4d656e4cef9aa468582e4ac17016e
shiv <defi.shiv@gmail.com>
2024-09-29 23:34:15 +0530

    mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Technologies from "./Technologies";
 
 function App() {
     return (
-        <div className="w-[100vw] px-5 py-5 xl:container xl:px-48 xl:py-14 font-mono">
+        <div className="w-[100vw] px-5 py-5 xl:mx-auto xl:container xl:px-48 xl:py-14 font-mono">
             <IntroDiv />
             <Education />
             <Technologies />
