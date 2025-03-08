a701adbcfee684be9efcd999b08fbe818c119d09
shiv <defi.shiv@gmail.com>
2025-03-08 08:52:34 +0530

    gradient added

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import Technologies from "./Technologies";
 
 function App() {
     return (
-        <div className="w-[100vw] px-5 py-5 xl:mx-auto xl:container xl:px-48 xl:py-14 font-mono">
-            <IntroDiv />
-            <Education />
-            <Technologies />
-            <Projects />
+        <div className="bg-gradient-to-tr from-black via-slate-800 to-black">
+            <div className="w-full py-5 xl:mx-auto xl:container xl:px-48 xl:py-14 font-mono">
+                <IntroDiv />
+                <Education />
+                <Technologies />
+                <Projects />
+            </div>
         </div>
     );
 }
